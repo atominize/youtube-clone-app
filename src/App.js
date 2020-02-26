6ab383f5bab6c762d6974bd32f264420e3dc7956
atominize <atominize@gmail.com>
2020-02-26 17:58:58 +0000

    Added api and components, and dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
+import { Grid } from '@material-ui/core';
+
+// import youtube from './api/youtube';
+
 class App extends React.Component {
   render(){
     return(
-      <h1>YouTube Clone App</h1>
+      <Grid justify="center" container spacing={16}>
+        <Grid item xs={12}>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              {/* Search bar */}
+            </Grid>
+            <Grid item xs={8}>
+              {/* Video Details */}
+            </Grid>
+            <Grid item xs={4}>
+              {/* Video List */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     )
   }
 }
